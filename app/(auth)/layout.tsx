1a8711c86628da00429c25eb165d08979a071e9b
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2023-09-04 22:09:18 +0000

    update

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ const AuthLayout = ({
     children: React.ReactNode
 }) => {
     return ( 
-        <div className="flex items-center justify-center h-full">
+        <div className="flex items-center justify-center h-[100vh]">
             {children}
         </div>
     );
