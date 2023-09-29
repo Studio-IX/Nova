f56b4c051f835d2d95c87c67efdad80307f18c94
Braimah-Abiola <braimahabiola.official@gmail.com>
2023-09-29 10:23:09 +0000

    Changed sign up/sign in bg

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,9 @@
-const AuthLayout = ({
-    children
-}:{
-    children: React.ReactNode
-}) => {
-    return ( 
-        <div className="flex items-center justify-center h-[100vh]">
-            {children}
-        </div>
-    );
-}
- 
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex items-center justify-center h-[100vh] bg-bg">
+      {children}
+    </div>
+  );
+};
+
 export default AuthLayout;
