116581a5190a63b23be7848fa7a42f086084ccdb
Jojo Duke <45549237+JojoDuke@users.noreply.github.com>
2023-10-16 22:59:30 +0000

    Update page.tsx

--- app/(dashboard)/(routes)/dashboard/settings/page.tsx
+++ app/(dashboard)/(routes)/dashboard/settings/page.tsx
@@ -6,7 +6,7 @@ const SettingsPage = async () => {
 
   return ( 
     <div className="h-full p-4 space-y-2">
-      <h3 className="text-lg font-medium">Settings</h3>
+      <h3 className="text-lg font-medium text-white">Settings</h3>
       <div className="text-muted-foreground text-sm">
         {isPro ? "You are currently on a Pro plan." : "You are currently on a free plan."}
       </div>
