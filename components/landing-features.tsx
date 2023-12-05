e80d54c7951e5dcaf9032e1d694921fc1cac7670
Jojo Duke <45549237+JojoDuke@users.noreply.github.com>
2023-12-05 02:33:24 +0000

    Update landing-features.tsx

--- components/landing-features.tsx
+++ components/landing-features.tsx
@@ -15,7 +15,7 @@ export const LandingFeatures = () => {
       <div className="w-fit flex flex-row justify-between items-center px-2 py-2 rounded-full border border-[#8343F3] bg-[rgba(131,67,243,0.1)] transition-all z-10">
         <div className="flex flex-row items-center mr-4">
           <div className="relative h-5 w-5 mr-2">
-            <Image fill alt="Arrow right icon" src="/features.svg" className="filter hue-rotate-[260deg] brightness-110"/>
+            <Image fill alt="Arrow right icon" src="/features.svg" className="filter hue-rotate-[260deg] brightness-80"/>
           </div>
           <p className="text-[#8343F3]">Features</p>
         </div>
