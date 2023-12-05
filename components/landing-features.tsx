4e5f568eb33c4e8a50ed3caf4ac2df6d3161d9b8
Jojo Duke <45549237+JojoDuke@users.noreply.github.com>
2023-12-05 02:02:08 +0000

    Update landing-features.tsx

--- components/landing-features.tsx
+++ components/landing-features.tsx
@@ -11,12 +11,16 @@ export const LandingFeatures = () => {
       id="features"
       className="w-full flex flex-col z-20 bg-bg md:py-20 px-5 md:px-0 items-center"
     >
-      <div className="w-fit flex flex-row justify-between items-center px-4 py-2 rounded-full border border-primry bg-[#221611] hover:bg-[#3E281F] transition-all mt-40 z-10">
-        <div className="flex flex-row items-center">
+
+      <div className="w-fit flex flex-row justify-between items-center px-2 py-2 rounded-full border border-[#8343F3] bg-[rgba(131,67,243,0.1)] transition-all z-10">
+        <div className="flex flex-row items-center mr-4">
           <div className="relative h-5 w-5 mr-2">
             <Image fill alt="Arrow right icon" src="/features.svg" className="filter hue-rotate-[260deg] brightness-150"/>
           </div>
-          <p className="section_tag_title mr-1">Features</p>
+          <div className="bg-[#8343F3] rounded-full px-3 py-1 mr-2">
+            <p className="text-white">New</p>
+          </div>
+          <p className="text-[#8343F3]">Introducing personal bots</p>
         </div>
       </div>
 
