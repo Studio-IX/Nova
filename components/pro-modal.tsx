05742dd532c66b79e30c36c29ab0c0705a2fa8b3
Jojo Duke <45549237+JojoDuke@users.noreply.github.com>
2023-12-08 20:23:41 +0000

    Update pro-modal.tsx

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -33,7 +33,7 @@ export const ProModal = () => {
 
       //window.location.href = response.data.url;
 
-      window.location.href = "https://studioixagency.lemonsqueezy.com/checkout/buy/94dc56f6-6a23-4bf3-870c-6feedebaec7e";
+      window.location.href = "https://buy.stripe.com/9AQ7vafvL4j2b2E144";
     } catch (error) {
       toast({
         description: "Something went wrong",
